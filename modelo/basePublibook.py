47efa078a36249b96de7a57e2d1f2ad70d1abd76
root <root@ip-10-0-0-225.ec2.internal>
2019-10-11 00:54:26 +0000

    cambios

--- modelo/basePublibook.py
+++ modelo/basePublibook.py
@@ -1,25 +1,44 @@
 # coding: utf-8
-from sqlalchemy import Column, Integer, Numeric, String
+from sqlalchemy import Column, Float, ForeignKey, String, Table
+from sqlalchemy.dialects.mysql import INTEGER
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
-
 
 Base = declarative_base()
 metadata = Base.metadata
 
 
-class Sticker(Base):
-    __tablename__ = 'stickers'
-    __table_args__ = {'schema': 'proyecto1'}
-
-    idstickers = Column(Integer, primary_key=True)
-    precio = Column(Numeric(10, 2), nullable=False)
-    url = Column(String(200), nullable=False)
-    disponible = Column(Integer, nullable=False)
-
-
 class Usuario(Base):
-    __tablename__ = 'usuario'
-    __table_args__ = {'schema': 'proyecto1'}
+    __tablename__ = 'Usuario'
+    __table_args__ = {u'schema': 'house'}
 
-    correo = Column(String(200), primary_key=True)
-    contrasenia = Column(String(500), nullable=False)
+    idUsuario = Column(INTEGER(11), primary_key=True)
+    nombreUsuario = Column(String(45), nullable=False)
+    nombre = Column(String(45), nullable=False)
+    apellidoP = Column(String(45), nullable=False)
+    apellidoM = Column(String(45))
+    contrasenia = Column(String(45), nullable=False)
+    correo = Column(String(45), nullable=False)
+
+    VideoJuego = relationship(u'VideoJuego', secondary=u'house.Tener')
+
+
+class VideoJuego(Base):
+    __tablename__ = 'VideoJuego'
+    __table_args__ = {u'schema': 'house'}
+
+    idVideoJuego = Column(INTEGER(11), primary_key=True)
+    nombre = Column(String(45), nullable=False)
+    genero = Column(String(45))
+    descripcion = Column(String(45))
+    clasificacion = Column(String(45))
+    consola = Column(String(45))
+    precio = Column(Float(asdecimal=True), nullable=False)
+
+
+t_Tener = Table(
+    'Tener', metadata,
+    Column('Usuario_idUsuario', ForeignKey(u'house.Usuario.idUsuario'), primary_key=True, nullable=False, index=True),
+    Column('VideoJuego_idVideoJuego', ForeignKey(u'house.VideoJuego.idVideoJuego'), primary_key=True, nullable=False, index=True),
+    schema='house'
+)
